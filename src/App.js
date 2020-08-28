eeb119243815e012a34da9d04361d37172f83d08
MacBook Pro <Macbookpro@MacBooks-MacBook-Pro-5.local>
2020-08-29 02:07:45 +0700

    ant design setting advance

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
+import { Button } from 'antd';
+// import Button from 'antd/es/button'
+import {DownloadOutlined} from '@ant-design/icons'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Button type="default" loading={true} type="primary">Primary Button</Button>
+        <Button icon={<DownloadOutlined/>}>Button</Button>
     </div>
   );
 }
